b95982b0d71ab67e7c17222eb19f7908fc3bf668
Hector Herrera <odiepus@gmail.com>
2018-10-21 15:37:23 -0500

    forgot to add file

---
+++ reduceFunc.py
@@ -0,0 +1,25 @@
+import sys
+#dict to contain the tuple(key) and the 
+#sum of all counts(value)
+entries = {}
+
+#for each entry received
+for line in sys.stdin:
+    #strip the entry of added \n's etc
+    line = line.strip()
+
+    #split the line into key and val(count)
+    key, count = line.split("\t", 1)
+   
+    #check if key is in the dict 
+    if key not in entries:
+        #if key not in dict then add it and set the count
+        entries[key] = count
+    else: #else pull up the entry and add the count to the value it has already
+        entries[key] += count
+
+#print each entry and its full count 
+for key,count in entries.items():
+    print key, "\t", count
+
+
